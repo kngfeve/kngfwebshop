918e3a2a620e00762f28553d892e01e65b97a4e3
jdpnielsen <jdpnielsen@gmail.com>
2015-02-07 01:02:35 +0100

    jshint cleanup

--- client/components/productFactory/productFactory.service.js
+++ client/components/productFactory/productFactory.service.js
@@ -3,24 +3,23 @@
 angular.module('kngfwebshopApp')
   .factory('productFactory', function ($http) {
     /** https://docs.angularjs.org/guide/providers **/
-    var test = 'http://localhost:3000/';
     var _prodFactory = {};
 
     _prodFactory.getProducts = function() {
-      console.log("_prodFactory.getProducts");
+      console.log('_prodFactory.getProducts');
       return $http.get('/api/products');
     };
     _prodFactory.create = function(data) {
-      console.log("_prodFactory.create");
-      return $http.post('/api/products', data)
+      console.log('_prodFactory.create');
+      return $http.post('/api/products', data);
     };
     _prodFactory.updateData = function(id, data) {
-      console.log("_prodFactory.updateData " + id);
-      return $http.put('/api/products/' + id, data)
+      console.log('_prodFactory.updateData ' + id);
+      return $http.put('/api/products/' + id, data);
     };    
     _prodFactory.deleteData = function(id) {
-      console.log("_prodFactory.deleteData " + id);
-      return $http.delete('/api/products/' + id)
+      console.log('_prodFactory.deleteData ' + id);
+      return $http.delete('/api/products/' + id);
     };          
     return _prodFactory;
   });
